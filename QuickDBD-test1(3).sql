b3476ffa9d428641975dbf73b0aff1a37a2b2ec2
remarkd123 <115809742+remarkd123@users.noreply.github.com>
2023-01-26 20:51:36 -0500

    completed
    complete

---
+++ QuickDBD-test1(3).sql
@@ -0,0 +1,60 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/QyHJBW
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "category" (
+    "category_id" varchar(5)   NOT NULL,
+    "vategory" varchar(50)   NOT NULL,
+    CONSTRAINT "pk_category" PRIMARY KEY (
+        "category_id"
+     )
+);
+
+CREATE TABLE "subcategory" (
+    "subcategory_id" varchar(10)   NOT NULL,
+    "subcategory" varchar(50)   NOT NULL,
+    CONSTRAINT "pk_subcategory" PRIMARY KEY (
+        "subcategory_id"
+     )
+);
+
+CREATE TABLE "crowdfunding" (
+    "cf_id" varchar(10)   NOT NULL,
+    "contact_id" varchar(10)   NOT NULL,
+    "company_name" varchar(50)   NOT NULL,
+    "description" varchar(100)   NOT NULL,
+    "goal" double(20)   NOT NULL,
+    "pledged" double(20)   NOT NULL,
+    "outcome" varchar(20)   NOT NULL,
+    "backers_count" double(10)   NOT NULL,
+    "country" varchar(50)   NOT NULL,
+    "currency" double(10)   NOT NULL,
+    "launch_date" datetime   NOT NULL,
+    "end_date" datetime   NOT NULL,
+    "category_id" varchar(10)   NOT NULL,
+    "subcategory_id" varchar(10)   NOT NULL,
+    CONSTRAINT "pk_crowdfunding" PRIMARY KEY (
+        "cf_id"
+     )
+);
+
+CREATE TABLE "contacts" (
+    "contact_id" varchar(10)   NOT NULL,
+    "firstname" varchar(50)   NOT NULL,
+    "lastname" varchar(50)   NOT NULL,
+    "email" varchar(50)   NOT NULL,
+    CONSTRAINT "pk_contacts" PRIMARY KEY (
+        "contact_id"
+     )
+);
+
+ALTER TABLE "category" ADD CONSTRAINT "fk_category_category_id" FOREIGN KEY("category_id")
+REFERENCES "crowdfunding" ("category_id");
+
+ALTER TABLE "subcategory" ADD CONSTRAINT "fk_subcategory_subcategory_id" FOREIGN KEY("subcategory_id")
+REFERENCES "crowdfunding" ("subcategory_id");
+
+ALTER TABLE "contacts" ADD CONSTRAINT "fk_contacts_contact_id" FOREIGN KEY("contact_id")
+REFERENCES "crowdfunding" ("contact_id");
+
---
+++ crowdfunding_db_schema.sql
@@ -0,0 +1,43 @@
+CREATE TABLE contacts (
+    contact_id varchar(10) not null,
+    first_name varchar(50) not null,
+    last_name varchar(50) not null,
+    email varchar(50),
+    primary key (contact_id)
+);
+
+CREATE TABLE subcategory (
+    subcategory_id varchar(10) not null,
+    subcategory varchar(50) not null,
+    primary key (subcategory_id)
+);
+
+CREATE TABLE category (
+    category_id varchar(10) not null,
+    category varchar(50) not null,
+    primary key (category_id)
+);
+
+CREATE TABLE campaign (
+    cd_id varchar(10),
+    contact_id varchar(10),
+    company_name varchar(100),
+    description varchar(100),
+    goal varchar(20),
+    pledged varchar(20),
+    outcome varchar(10),
+    backers_count varchar(10),
+    country varchar(50),
+    currency varchar(20),
+    launch_date date,
+    end_date date,
+    category_id varchar(10),
+    subcategory_id varchar(10),
+    primary key(cd_id)
+);
+
+ALTER TABLE campaign ADD CONSTRAINT fk_camp_contact_id FOREIGN KEY(contact_id) REFERENCES contacts(contact_id);
+
+ALTER TABLE campaign ADD CONSTRAINT fk_camp_subcat_id FOREIGN KEY(subcategory_id) REFERENCES subcategory(subcategory_id);
+
+ALTER TABLE campaign ADD CONSTRAINT fk_camp_cat_id FOREIGN KEY(category_id) REFERENCES category(category_id);
